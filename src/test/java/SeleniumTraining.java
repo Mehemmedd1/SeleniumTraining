a3fac4757b757b78b62097dea4bc4f36d6fa6ad9
Mehemmedd1 <mehemmed0315@gmail.com>
2026-09-10 22:48:55 +0400

    Selenium

--- src/test/java/SeleniumTraining.java
+++ src/test/java/SeleniumTraining.java
@@ -4,19 +4,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumTraining {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com");
 
         driver.manage().window().maximize();
-        driver.manage().window().minimize();
-        driver.manage().window().fullscreen();
+        Thread.sleep(2000);
         driver.navigate().to("https://demoqa.com/");
         driver.navigate().back();
         driver.navigate().forward();
-        driver.navigate().refresh();
         driver.navigate().to("https://demoqa.com/text-box");
         driver.findElement(By.id("userName")).sendKeys("John Doe");
+        driver.findElement(By.id("userEmail")).sendKeys("john.doe@example.com");
+        driver.findElement(By.id("currentAddress")).sendKeys("123 Main St, Anytown, USA");
+        driver.findElement(By.id("permanentAddress")).sendKeys("456 Elm St, Anytown, USA");
+        driver.findElement(By.id("submit")).click();
+        Thread.sleep(2000);
+
         driver.quit();
 
     }
---
+++ src/test/java/TestNG.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test
+    public void test1(){
+        System.out.println("Test 1");
+    }
+}
